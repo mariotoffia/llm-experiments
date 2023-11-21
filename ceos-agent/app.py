d514896d061d86da353a3c9ca37d8e5776af45c3
Mario Toffia <mario.toffia@gmail.com>
2023-11-21 15:19:23 +0100

    handle settings properly

--- ceos-agent/app.py
+++ ceos-agent/app.py
@@ -4,15 +4,10 @@ from embeddingsdb import EmbeddingsDb
 from dotenv import load_dotenv
 from langchain.pydantic_v1 import BaseModel
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.chat_models import ChatOpenAI
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import Runnable, RunnableParallel, RunnablePassthrough, RunnableConfig
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
+from langchain.prompts.chat import ChatPromptTemplate
 
 import chainlit as cl
 from chainlit.input_widget import Select, Switch, Slider
@@ -21,13 +16,11 @@ from chainlit.input_widget import Select, Switch, Slider
 load_dotenv()
 
 # Initialize System
+ceos_user = "mario.toffia@crossbreed.se"
 user_file_path = os.path.join(os.path.dirname(__file__), "data", "user")
 
 if not os.path.exists(user_file_path):
     os.makedirs(user_file_path)
-
-# LLM
-model = ChatOpenAI(model_name="gpt-3.5-turbo", streaming=True, temperature=0)
 
 # Embeddings
 embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
@@ -44,28 +37,50 @@ Question: {question}
 
 prompt = ChatPromptTemplate.from_template(template)
 
-## TODO: QnA Chain
-# Create a chain that uses the Chroma vector store for RAG
-# chain = RetrievalQAWithSourcesChain.from_chain_type(
-#     ChatOpenAI(temperature=0, streaming=True),
-#     chain_type="stuff",
-#     retriever=embeddings_db.as_retriever(),
-# )
 
-# Add typing for input
 class Question(BaseModel):
     __root__: str
 
 
+def setup_chain(model: str, temperature: float, streaming: bool) -> any:
+    """
+    Setup the LLM model and the chain.
+
+    :param model: The model name
+    :param temp: The temperature
+    :param streaming: The streaming flag
+    :return: The chain
+    """
+    chat_model = ChatOpenAI(
+        model_name=model,
+        streaming=streaming,
+        temperature=temperature,
+    )
+    
+    chain = (
+        {"context": embeddings_db.as_retriever(), "question": RunnablePassthrough()}
+        | prompt
+        | chat_model
+        | StrOutputParser()
+    )
+
+    return chain.with_types(input_type=Question)
+
+
 @cl.on_chat_start
 async def on_chat_start():
-    ettings = await cl.ChatSettings(
+    # Set the chain into the user session
+    chain = setup_chain(model="gpt-4", temperature=0.0, streaming=True)
+    cl.user_session.set("chain", chain.with_types(input_type=Question))
+
+    await cl.ChatSettings(
         [
             Select(
                 id="Model",
                 label="OpenAI - Model",
-                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
-                initial_index=0,
+                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k",
+                        "gpt-4", "gpt-4-32k"],
+                initial_index=2,
             ),
             Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
             Slider(
@@ -79,32 +94,28 @@ async def on_chat_start():
         ]
     ).send()
 
-    # Set the chain into the user session
-    chain = (
-        {"context": embeddings_db.as_retriever(), "question": RunnablePassthrough()}
-        | prompt
-        | model
-        | StrOutputParser()
-    )
-
-    chain = chain.with_types(input_type=Question)
-
-    cl.user_session.set("chain", chain)
+    await cl.Avatar(
+        name=ceos_user,
+        url="https://avatars.githubusercontent.com/u/1716527?v=4"
+    ).send()
 
     # Sending an image...
     elements = [
         cl.Image(
             name="CEOS",
-            display="inline",            
+            display="inline",
             url="https://www.crossbreed.se/wp-content/uploads/2021/06/energyos-top-text-mobile-webb.jpg",
         ),
     ]
 
-    await cl.Message(content="",elements=elements).send()
-    await cl.Message(content="Welcome to Crossbreed Energy OS. Please ask me a question!").send()
+    await cl.Message(content="", author=ceos_user, elements=elements).send()
+    await cl.Message(
+        content="Welcome to Crossbreed Energy OS. Please ask me a question!",
+        author=ceos_user,
+    ).send()
 
     # files = None
- 
+
     # Wait for the user to upload a file
     # while files == None:
     #     files = await cl.AskFileMessage(
@@ -127,15 +138,25 @@ async def on_chat_start():
     #     f.write(text)
 
     # user_index(embeddings=embeddings_db)
-  
+
     # # Let the user know that the system is ready
     # msg.content = f"Processing `{file.name}` done!"
 
-    # await msg.update()    
+    # await msg.update()
 
+
+@cl.on_settings_update
+async def setup_agent(settings):
+  chain = setup_chain(
+      model=settings["Model"], 
+      temperature=settings["Temperature"], 
+      streaming=settings["Streaming"],
+  )
+
+  cl.user_session.set("chain", chain.with_types(input_type=Question))
 
 @cl.on_message
-async def on_message(message: cl.Message):    
+async def on_message(message: cl.Message):
     runnable = cl.user_session.get("chain")  # type: Runnable
 
     if runnable is None:
@@ -144,7 +165,7 @@ async def on_message(message: cl.Message):
         return
 
     # clear message
-    msg = cl.Message(content="")
+    msg = cl.Message(content="", author=ceos_user)
     await msg.send()
 
     async for chunk in runnable.astream(

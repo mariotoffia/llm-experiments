4a916f669f9f0b487779ac2a32d867dac5dfac5e
Mario Toffia <mario.toffia@gmail.com>
2023-11-21 09:22:22 +0100

    text extraction for training

--- ceos-agent/app.py
+++ ceos-agent/app.py
@@ -1,9 +1,56 @@
 from langchain.document_loaders import UnstructuredFileLoader
+from unstructured.cleaners.core import clean_extra_whitespace
 from langchain.document_loaders import UnstructuredURLLoader
 
-loader = UnstructuredFileLoader("app.py")
+# https://medium.com/@onkarmishra/using-langchain-for-question-answering-on-own-data-3af0a82789ed
+
+loader = UnstructuredFileLoader(
+    file_path="data/training/smarter-heating.md",
+    strategy="hi-res", # other option:"fast"
+    mode="elements", # single (default), elements, paged (for PDFs)
+    post_processors=[clean_extra_whitespace],
+)
+
+# loader = UnstructuredURLLoader(
+#    urls=["https://www.crossbreed.se"], continue_on_failure=True, show_progress_bar=False, mode="elements"//
+# )
 
 docs = loader.load()
 
-print(docs)
+result = []
+current_question = None
+current_answer = []
 
+for doc in docs:
+    # Check if metadata and category exist, otherwise treat content as part of the answer
+    category = doc.metadata.get("category") if doc.metadata else None
+    content = doc.page_content.strip()
+
+    if category is None:
+        result.append({"Answer": doc.page_content.strip(), "Metadata": doc.metadata})
+        continue
+
+    if category == "Title":
+        # If there is an ongoing question, save it before starting a new one
+        if current_question is not None:
+            result.append({"Question": current_question, "Answer": "\n".join(current_answer), "Metadata": doc.metadata})
+            current_answer = []
+
+        current_question = content        
+    else:
+        if category == "ListItem":
+            current_answer.append(f"• {content}")  # Format list items with a bullet
+        else:
+            # Treat as part of the answer if there's no category or it's not a ListItem
+            current_answer.append(content)
+
+# Don't forget to add the last question-answer pair
+if current_question is not None:
+    result.append({"Question": current_question, "Answer": "\n".join(current_answer), "Metadata": doc.metadata})
+
+for res in result:
+    if res.get("Question") is not None:
+      print(res.get("Question"))
+      print("-" * 50)
+    print(res.get("Answer"))
+    print("\n")
---
+++ ceos-agent/data/training/smarter-heating.md
@@ -0,0 +1,29 @@
+# What is Crossbreed Smarter Heating?
+Crossbreed Smarter Heating is a optimization servicer that allows optimization of heat consumption up to 15%. This is done by using the building as a thermal storage and weather predictions. The service is available for both residential and commercial buildings.
+
+It runs on Crossbreed Energy OS, _CEOS_, and is available as a service.
+
+## I have building that I want to optimize, what do I do?
+You need to provide with the following information:
+
+* What type of controlling equipment do you have? Siemens, Schneider, etc.
+* What type of indoor climate sensors do you have? Wideco, Telia, Sensitive etc.
+* How to access your equipment.
+
+## What do I need to tell you in order to access my equipment?
+You need to provide with the following information:
+
+* The type of system and the identifiers for _CEOS_ to determine how to access it. Depending on the vendor it may be different.
+
+## I have a siemens system, what do I need to provide?
+You need to provide with the following information:
+
+* The Tenant ID, for example: "T123".
+* The Plant ID, for example "Plant 1".
+* Which profile you want to use. Default is "generic".
+
+## The indoor climate sensors are from Sensitive, what do I need to provide?
+You need to provide with the following information:
+
+* The sensor ID, for example: "S14".
+* The Tenant ID, for example "myAccount".
